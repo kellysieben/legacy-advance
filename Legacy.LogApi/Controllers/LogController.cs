ef3d122a354b369a6061703031ba33674218ce72
ksieben <ksieben@D25V1CH2>
2017-05-17 17:29:00 -0600

    Added InMemoryLog (non-persisted) data service.

--- Legacy.LogApi/Controllers/LogController.cs
+++ Legacy.LogApi/Controllers/LogController.cs
@@ -1,35 +1,38 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Web.Http;
+using Legacy.LogApi.Data;
 
 namespace Legacy.LogApi.Controllers
 {
     public class LogController : ApiController
     {
-        private readonly List<string> _vals = new List<string>();
+        private readonly InMemoryLog _vals = new InMemoryLog();
 
         // GET: api/Log
         public IEnumerable<string> Get()
         {
-            return _vals;
+            return _vals.Get();
         }
 
         // GET: api/Log/5
         public string Get(int id)
         {
-            return _vals[id];
+            return "not implemented";
         }
 
         // POST: api/Log
         public void Post([FromBody]string value)
         {
-            _vals.Add(value);
+            _vals.Add("<" + DateTime.Now + "> " + value);
+            //return CreatedAtRoute();
         }
 
         // PUT: api/Log/5
-        public void Put(int id, [FromBody]string value)
-        {
-            _vals[id] = value;
-        }
+//        public void Put(int id, [FromBody]string value)
+//        {
+//            _vals[id] = value;
+//        }
 
         // DELETE: api/Log/5
         public void Delete(int id)
---
+++ Legacy.LogApi/Data/InMemoryLog.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Legacy.LogApi.Data
+{
+    public class InMemoryLog
+    {
+        private static readonly List<string> All = new List<string>();
+
+        public void Add(string entry)
+        {
+            All.Add(entry);
+        }
+
+        public List<string> Get() => All;
+    }
+}

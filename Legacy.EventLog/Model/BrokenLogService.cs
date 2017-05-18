5906477eeb38b2c8c7612b5ba9bcbc188b30e254
ksieben <ksieben@D25V1CH2>
2017-05-18 14:24:39 -0600

    Started new log services.

---
+++ Legacy.EventLog/Model/BrokenLogService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Legacy.EventLog.Model
+{
+    public class BrokenLogService : ILogService
+    {
+        public void AddNewEntry(string newEntry)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<string> GetAllEntries()
+        {
+            return new List<string>() {"<" + DateTime.Now + "> Don't even. It broke!"};
+        }
+    }
+}
---
+++ Legacy.EventLog/Model/RemoteLogService.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Legacy.EventLog.Model
+{
+    public class RemoteLogService : ILogService
+    {
+        public void AddNewEntry(string newEntry)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<string> GetAllEntries()
+        {
+            return new List<string>() {"<1/1/1970> Nothing to see here"};
+        }
+    }
+}
